9b0ec8426462f4bb80630b0b761248950cf518f4
Amy Liu <liu.amy.tech@gmail.com>
2017-03-17 12:25:13 -0500

    Initial version

---
+++ str.c
@@ -0,0 +1,59 @@
+// String operations
+
+#include <stdio.h>
+#include <string.h>
+
+
+void reverse_str(char *str, int len)
+{
+  #if 1
+  int i, j;
+  char ch;
+
+  for (i = 0, j = len -1; i < j; i++, j--) {
+    ch = str[i];
+    str[i] = str[j];
+    str[j] = ch;
+  }
+  #endif
+
+  #if 0
+  char *end = str + len - 1;
+  char ch;
+
+  while (str < end) {
+    ch = *str;
+    *str++ = *end;
+    *end-- = ch;
+  }
+  #endif
+}
+
+void reverse_words_in_str(char *str)
+{
+  // First reverse the whole string
+  reverse_str(str, strlen(str));
+
+  // Reverse each word in the string
+  char *curr = str;
+  while (*curr) {
+    ++curr;
+    if (*curr == ' ') {
+      reverse_str(str, curr - str);
+      str = curr + 1;
+    }
+    else if (*curr == '\0') {
+      reverse_str(str, curr - str);
+    }
+  }
+}
+
+int main(void)
+{
+  char mystr[] = "Hello   world";
+
+  reverse_words_in_str(mystr);
+  printf("%s\n", mystr);
+
+  return 0;
+}
