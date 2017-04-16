5c2a19e4e229e150220723c3edff6bbc0fa9078c
Amy Liu <liu.amy.tech@gmail.com>
2017-04-15 22:33:04 -0500

    Add shift_mul().

--- bitops.c
+++ bitops.c
@@ -13,18 +13,19 @@ void swap_int(int *a, int *b)
   *a = *a ^ *b; // *a ^ *b ^ *b = *a
 }
 
-int count_set_bits(int a)
+// a is unsigned, so 0 is filled when a right shifts.
+int count_set_bits(unsigned int a)
 {
-  int i;
   int count = 0;
   
-  for (i = 0; a; i++) {
+  while (a) {
     if (a & 1) {
       count++;
     }
+
     a >>= 1;
   }
-
+  
   return count;
 }
 
@@ -33,7 +34,22 @@ int is_power_of_two(int x)
   return ((x & (x - 1)) == 0);
 }
 
+// Use shift to implement multiplication
+unsigned short shift_mul(unsigned char a, unsigned char b)
+{
+  unsigned short result = 0;
 
+  while (a) {
+    if (a & 1) {
+      result += b;
+    }
+
+    a >>= 1;
+    b <<= 1;
+  }
+
+  return result;
+}
 
 int main(void)
 {
@@ -49,10 +65,14 @@ int main(void)
   swap_int(&a, &b);
   printf("a: %d, b: %d\n", a, b);
 
-  printf("number of set bits: %d\n", count_set_bits(10));
+  a = 0xffffffff;
+  printf("0x%x has %d set bits\n", a, count_set_bits(a));
 
   x = 9;
-  printf("%d is power of 2: %d\n", x, is_power_of_two(x));
-  
+  printf("%d is power of 2: %d?\n", x, is_power_of_two(x));
+
+  a = 7;
+  b = 11;
+  printf("%d x %d is %d\n", a, b, shift_mul(a, b));
   return 0;
 }
