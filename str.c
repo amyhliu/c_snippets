ff536c679055412599abc38c9f0dc39977113416
Amy Liu <liu.amy.tech@gmail.com>
2017-10-06 11:12:14 -0500

    Fixed errors when there are spaces at the end of the original string.

--- str.c
+++ str.c
@@ -1,59 +1,70 @@
 // String operations
-
 #include <stdio.h>
 #include <string.h>
-
 
 void reverse_str(char *str, int len)
 {
 #if 1
-  int i, j;
-  char ch;
+	int i, j;
+	char ch;
 
-  for (i = 0, j = len -1; i < j; i++, j--) {
-    ch = str[i];
-    str[i] = str[j];
-    str[j] = ch;
-  }
+	for (i = 0, j = len -1; i < j; i++, j--) {
+		ch = str[i];
+		str[i] = str[j];
+		str[j] = ch;
+	}
+#else
+	char *end = str + len - 1;
+	char ch;
+
+	while (str < end) {
+		ch = *str;
+		*str++ = *end;
+		*end-- = ch;
+	}
 #endif
+}
 
-#if 0
-  char *end = str + len - 1;
-  char ch;
 
-  while (str < end) {
-    ch = *str;
-    *str++ = *end;
-    *end-- = ch;
-  }
-#endif
+char* skip_space(char **p)
+{
+	while (**p == ' ') {
+		(*p)++;
+	}
+
+	return *p;
 }
 
 void reverse_words_in_str(char *str)
 {
-  // First reverse the whole string
-  reverse_str(str, strlen(str));
+	// First reverse the whole string
+	reverse_str(str, strlen(str));
 
-  // Reverse each word in the string
-  char *curr = str;
-  while (*curr) {
-    ++curr;
-    if (*curr == ' ') {
-      reverse_str(str, curr - str);
-      str = curr + 1;
-    }
-    else if (*curr == '\0') {
-      reverse_str(str, curr - str);
-    }
-  }
+	// Reverse each word in the string
+	char *curr = str;
+
+	str = skip_space(&curr);
+	while (*curr) {
+		++curr;
+		if (*curr == ' ') {
+			reverse_str(str, curr - str);
+			str = skip_space(&curr);
+		}
+		else if ((*curr == '\0')) {
+			reverse_str(str, curr - str);
+		}
+	}
 }
 
 int main(void)
 {
-  char mystr[] = "Hello   world";
+	char mystr[] = "    Hello   world ";
+	printf("Orignial string:\n");
+	printf("%s, length: %zd\n", mystr, strlen(mystr));
 
-  reverse_words_in_str(mystr);
-  printf("%s\n", mystr);
+	reverse_words_in_str(mystr);
+	printf("Reversed string: \n");
+	printf("%s, length: %zd\n", mystr, strlen(mystr));
 
   return 0;
 }
